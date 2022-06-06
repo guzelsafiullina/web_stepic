95bad1a4e547ebe864b70e31085254f9c9f8eaf0
guzelsafiullina <89734522+guzelsafiullina@users.noreply.github.com>
2022-06-06 11:35:04 +0300

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,7 @@
+def application(environ, start_response):
+    status = '200 OK'
+    headers = [
+        ('Content-Type', 'text/plain')
+    ]
+    start_response(status, headers)
+    return [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
